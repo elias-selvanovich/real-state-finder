bd6631468dd072c2b2f367e6f0257c12e3ca4e52
Elias Selvanovich <elias.selvanovich@gmail.com>
2023-06-11 11:58:11 -0300

    Add validation

--- examples/authenticate.sh
+++ examples/authenticate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -z $APP_ID ]; then
+  echo "APP_ID is empty, please set it before running this command"
+  exit
+fi
+
 echo "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=$APP_ID&redirect_uri=https://localhost:8080/"
